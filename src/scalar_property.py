39bd73ac426fb3146e100375c3df53f50546daf7
Mary-Ann Maheswaran <maryann.thyveetil@gmail.com>
2023-02-02 23:12:45 +0000

    Made sure el2nodmap private now to mesh.

--- src/scalar_property.py
+++ src/scalar_property.py
@@ -40,8 +40,8 @@ class Volume(ScalarProperty):
     def __str__(self):
         return 'Volume'
     
-    def update(self, ndpositions, el2nodemap, mesh):
+    def update(self, ndpositions, mesh):
         noelements = len(self.data)
         for e in range(noelements):
-            self.data[e] = mesh.get_volume(e,el2nodemap,ndpositions)
+            self.data[e] = mesh.get_volume(e,ndpositions)
             
